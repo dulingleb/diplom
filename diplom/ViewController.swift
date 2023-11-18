651ea7247b52d190f80da09b35d9c983882e40fb
dulingleb <dulingleb@gmail.com>
2023-11-18 21:42:01 +0100

    Initial Commit

---
+++ diplom/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  diplom
+//
+//  Created by Dulin Gleb on 18.11.23..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
