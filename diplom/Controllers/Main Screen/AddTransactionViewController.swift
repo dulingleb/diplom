2c5f7577d34b870cd9c7f2fb86ff88f19603c449
dulingleb <dulingleb@gmail.com>
2023-12-05 14:50:20 +0100

    keyboard

---
+++ diplom/Controllers/Main Screen/AddTransactionViewController.swift
@@ -0,0 +1,31 @@
+//
+//  MainViewController.swift
+//  diplom
+//
+//  Created by Dulin Gleb on 27.11.23..
+//
+
+import UIKit
+
+class AddTransactionViewController: UITabBarController {
+    
+    @IBOutlet weak var transactionTypeSegmentController: UISegmentedControl!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ diplom/Controllers/Main Screen/TransactionTypeSegmentController.swift
@@ -0,0 +1,20 @@
+//
+//  TransactionTypeSegmentController.swift
+//  diplom
+//
+//  Created by Dulin Gleb on 27.11.23..
+//
+
+import UIKit
+
+class TransactionTypeSegmentController: UISegmentedControl {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
