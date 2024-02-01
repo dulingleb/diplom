3a262db91e0ceb5b613ce7546bf4336cfc94fdda
dulingleb <dulingleb@gmail.com>
2024-02-02 00:00:34 +0100

    Accounts tab

---
+++ diplom/Controllers/Main Screen/TransactionCategory/TransactionCategoryCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  TransactionCategoryCollectionViewCell.swift
+//  diplom
+//
+//  Created by Dulin Gleb on 30.1.24..
+//
+
+import UIKit
+
+class TransactionCategoryCollectionViewCell: UICollectionViewCell {
+    
+}
